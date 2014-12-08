d1fdb81528e3d2743339f036da7ad4d5a05442ee
Nam Tran <namtv.it07@gmail.com>
2014-12-08 12:25:12 +0700

    Fix clockwork config on server

--- config/clockwork.rb
+++ config/clockwork.rb
@@ -1,6 +1,9 @@
+app_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+$LOAD_PATH.unshift(app_path) unless $LOAD_PATH.include?(app_path)
+
 require 'clockwork'
-require './config/boot'
-require './config/environment'
+require 'config/boot'
+require 'config/environment'
 module Clockwork
   handler do |job|
     puts "Running #{job}"
@@ -8,3 +11,4 @@ module Clockwork
 
   every(10.seconds, 'End inactive channels') {Channel.end_inactive_channels}
 end
+
